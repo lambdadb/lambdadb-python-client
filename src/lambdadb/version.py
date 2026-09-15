ad732053bcca0b3d065c2ae57063c7805012db31
Steven Kim <swkim86@gmail.com>
2026-09-15 20:48:11 +0900

    chore: prepare 0.9.0rc3 release

--- src/lambdadb/version.py
+++ src/lambdadb/version.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 TITLE: str = "lambdadb"
-SDK_VERSION: str = "0.9.0.dev2"
+SDK_VERSION: str = "0.9.0rc3"
 OPENAPI_DOC_VERSION: str = "1.1.1"
 GEN_VERSION: str = "2.798.0"
 API_CONTRACT_REVISION: str = "c8495bf47cd8918cfd546b4742823fd4cf3d0814"
